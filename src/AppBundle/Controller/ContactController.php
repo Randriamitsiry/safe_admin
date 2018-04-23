036c79ba079d94469efb3655e7f3f906a36d0db1
jessGabriel <grandriamitsiry@bocasay.com>
2018-04-23 09:43:37 +0200

    contact message

---
+++ src/AppBundle/Controller/ContactController.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jess
+ * Date: 23/04/18
+ * Time: 08:42
+ */
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Contact;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+class ContactController extends Controller
+{
+    /**
+     * @Route("/send/message", name="send_message")
+     * @return Response
+     * @param Request
+     * @Method("POST")
+     */
+    public function sendAction(Request $request)
+    {
+        $mailer = $this->get("mailer");
+        try {
+            $contact = new Contact();
+            $contact->setNom($request->request->get("nom"));
+            $contact->setMessage($request->request->get("message"));
+            $contact->setEmail($request->request->get("email"));
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($contact);
+            $em->flush();
+
+            $message = (new \Swift_Message("Message de contact"));
+            $header = $message->embed(\Swift_Image::fromPath("img/head.png"));
+            $message->setFrom($request->request->get("email"))
+                ->setTo("tsiryjessgabriel@gmail.com")
+                ->setBody(
+                    $this->renderView(
+                        "emails/contact_message.html.twig", array("contact"=>$contact, "header"=>$header)
+                    ), "text/html"
+                );
+            $mailer->send($message);
+
+            return new Response("OKAY");
+        } catch (\Exception $exception) {
+            throw $exception;
+        }
+    }
+}
---
+++ src/AppBundle/Entity/Contact.php
@@ -0,0 +1,167 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Contact
+ * @ORM\HasLifecycleCallbacks()
+ * @ORM\Table(name="contact")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ContactRepository")
+ */
+class Contact extends Controller
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=100)
+     */
+    private $nom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=100)
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="message", type="string", length=255)
+     */
+    private $message;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date_envoi", type="datetime")
+     */
+    private $dateEnvoi;
+
+
+    /**
+     * Get id.
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nom.
+     *
+     * @param string $nom
+     *
+     * @return Contact
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom.
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set email.
+     *
+     * @param string $email
+     *
+     * @return Contact
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email.
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set message.
+     *
+     * @param string $message
+     *
+     * @return Contact
+     */
+    public function setMessage($message)
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    /**
+     * Get message.
+     *
+     * @return string
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    /**
+     * Set dateEnvoi.
+     *
+     * @param \DateTime $dateEnvoi
+     *
+     * @return Contact
+     */
+    public function setDateEnvoi($dateEnvoi)
+    {
+        $this->dateEnvoi = $dateEnvoi;
+
+        return $this;
+    }
+
+    /**
+     * Get dateEnvoi.
+     *
+     * @return \DateTime
+     */
+    public function getDateEnvoi()
+    {
+        return $this->dateEnvoi;
+    }
+
+    /**
+     * @ORM\PrePersist
+     */
+    public function beforePersist()
+    {
+        $this->setDateEnvoi(new \DateTime("now"));
+    }
+}
