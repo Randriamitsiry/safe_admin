26214d87e172e59e388f1bc12e3d51a07ac98d10
jessGabriel <grandriamitsiry@bocasay.com>
2018-04-25 13:59:28 +0200

    contact

---
+++ src/AppBundle/Repository/ContactRepository.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jess
+ * Date: 23/04/18
+ * Time: 09:54
+ */
+
+namespace AppBundle\Repository;
+
+
+use Doctrine\ORM\EntityRepository;
+
+class ContactRepository extends EntityRepository
+{
+
+}
